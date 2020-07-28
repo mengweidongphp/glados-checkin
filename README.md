cca9caa93bd8ca18c0a3ee6fa311135e20a94df5
supermantou <458744230@qq.com>
2020-07-28 21:11:44 +0800

    更新说明

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # glados-checkin
-  每天自动签到（天数+1），自动推送结果  
+  每天自动签到（天数+=1），使用Server酱推送结果到微信
 
 # Server酱：
 负责推送消息到微信。
@@ -16,7 +16,7 @@
 **SERVE**  ——**on/off** 你想你的serve酱开不开启通知  
 **SCKEY**  ——**sckey**  开的话填你的serve酱的sckey，不开就不填   
 **COOKIE** —— **cookie** 弄上你账号的cookie  
-暂不支持多账号，懒得弄
+
 ![](http://tu.yaohuo.me/imgs/2020/06/748bf9c0ca6143cd.png)
 
 ## 三、启用Action
@@ -28,7 +28,7 @@
 Actions > Cloud189Checkin > build  
 能看到如下图所示，表示成功，或者看你微信通知  
 ![](http://tu.yaohuo.me/imgs/2020/06/289432b53bded61c.png)  
-  
+
 # 修改签到时间
 可以在[.github/workflows/python-publish.yml]中修改  
 ```
@@ -36,5 +36,26 @@ on:
   schedule:
     - cron cron表达式
 ```
-`0 9 * * * `表示每天9点启动，在GitHub的编辑页面中会有如下提示
+`0 9 * * * ` 表示每天9点启动，在GitHub的编辑页面中会有如下提示
 ![](https://raw.githubusercontent.com/458744230/pic/master/img/20200727230953.png)
+
+注意时间是UTC时间，如果你在东八区，9表示每天9+8=17点签到
+
+# 获取Cookies
+
+以chrome为例
+
+登录到web端,打开F12，依次点击Application，Cookies以查看所有的cookies，编辑取消所有的HttpOnly
+
+![](https://cdn.jsdelivr.net/gh/458744230/pic/img/20200728205916.png)
+
+然后再console界面输入document.cookie，就能获得文本形式所有的cookie，注意不带引号
+
+
+
+# Reference
+
+[glados-checkin](https://github.com/xiaomustudent/glados-checkin)
+
+[GLaDOS](https://github.com/glados-network/GLaDOS)
+
--- checkin.py
+++ checkin.py
@@ -16,21 +16,37 @@ def start():
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-   # print(res)
-    #print(checkin.json())
-    #print(state.json())
-    if 'message' in checkin.text:
-        mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        #print(time)
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+    if not sckey :
+        sever = False
     else:
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        sever = sever.lower() == 'on'
+
+    try:
+        checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+        state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
+    except Exception as e:
+        print('Please check if env variable correct')
+        print(e)
+        raise e
+    
+
+    # print(res)
+
+    try:
+        if 'message' in checkin.text:
+            mess = checkin.json()['message']
+            time = state.json()['data']['leftDays']
+            time = time.split('.')[0]
+            #print(time)
+            sever & requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+        else:
+            sever & requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+    except Exception as e:
+        print(checkin.json())
+        print(state.json())
+        sever & requests.get('https://sc.ftqq.com/' + sckey + '.send?text=签到异常')
+        raise e
+
 
 def main_handler(event, context):
   return start()
